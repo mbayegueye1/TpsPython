d14d747799001faae0bc3e3bfe2e4fb9b0531f46
bayfall <mbaye1gueye@outlook.com>
2023-12-08 11:03:21 +0100

    TP2 raname

---
+++ Approche imp/EX02_INVERSIONCONTENU.py
@@ -0,0 +1,5 @@
+liste=[1,15,-3,0,8,7,4,-2,28,7,-1,17,2,3,0,14,-4]
+liste.reverse()
+print("l’ensemble des éléments de la liste listeCopie")
+listeCopie = liste.copy()
+print(listeCopie)
---
+++ Approche imp/ex01_AffichageListe.py
@@ -0,0 +1,10 @@
+liste=[1,15,-3,0,8,7,4,-2,28,7,-1,17,2,3,0,14,-4]
+print("les elements de la liste:")
+for i in liste:
+    print(i)
+print("les éléments de la liste dans l’ordre inverse:")
+liste.reverse()
+for i in liste:
+    print(i)
+
+
