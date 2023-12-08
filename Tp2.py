3be5c0dbe31411b376396519fe65d6e54007d5c9
bayfall <mbaye1gueye@outlook.com>
2023-12-08 10:22:49 +0100

    TP2

---
+++ Tp2.py
@@ -0,0 +1,8 @@
+a= 2
+b= 38.3
+c= "KILLERS OF THE FLOWER MOON"
+d = True
+print("le type de la variable a=", a , "et", type(a))
+print("le type de la variable b=", b , "et", type(b))
+print("le type de la variable c=", c , "et", type(c))
+print("le type de la variable d=", d , "et", type(d))
