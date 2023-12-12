6ac0bc247b04d3f20af603188df2eb94a1b9a40d
bayfall <mbaye1gueye@outlook.com>
2023-12-12 17:08:21 +0100

    tp4 corrigé

--- Approche objet/tp4 – methode static.py
+++ Approche objet/tp4 – methode static.py
@@ -1,13 +1,11 @@
 class ChaineUtils:
     @staticmethod
-    def est_anagramme(chaine1, chaine2 ):
-        if len(chaine1) == len(chaine2):
-            for i in chaine1:
-                if i in chaine2:
-                    return "Oui Anagramme"
-                else:
-                    return "non pas des annagrammes"
-        return "les deux chaines n'ont pas la meme taille"
+    def est_anagramme(chaine1, chaine2):
+        if len(chaine1) == len(chaine2) and sorted(chaine1) == sorted(chaine1):
+           return "Oui Anagramme"
+        else:
+            return "non pas des annagrammes"
+
     @staticmethod
     def comptage_chaine(chaine1, chaine2):
         cnt = chaine2.count(chaine1)  ## compte le nombre de fois ou la chaine2 apparait dans la chaine1
